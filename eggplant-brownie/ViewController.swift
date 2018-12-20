8741c017f9e27421317db2e3c269183a21aa1a7a
Harold Barros <haroldbarros@gmail.com>
2018-12-20 18:06:33 -0200

    Initial Commit

---
+++ eggplant-brownie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by Harold Jose Barros Gonçalves on 20/12/18.
+//  Copyright © 2018 Rsac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
