304072984df754881b8a1e8e58b022384681880f
Harold Barros <haroldbarros@gmail.com>
2018-12-21 17:21:59 -0200

    Swift Aula 04

---
+++ eggplant-brownie/models/Meal.swift
@@ -0,0 +1,23 @@
+
+import Foundation
+
+
+class Meal {
+    let name:String
+    let happiness:Int
+    let itens = Array<Item>()
+    
+    init(name:String,happiness:Int) {
+        self.name = name;
+        self.happiness = happiness;
+    }
+    
+    func allCalories() -> Double {
+        var total:Double = 0.0
+        for item in self.itens {
+            total += item.calories
+        }
+        
+        return total
+    }
+}
--- eggplant-brownie/viewcontrollers/ViewController.swift
+++ eggplant-brownie/viewcontrollers/ViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet var nameField: UITextField!
+    
+    @IBOutlet var happinesField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func add(){
+        let name = nameField.text
+        let happiness = happinesField.text
+        print("eaten \(name) with happiness \(happiness)!")
+        
+
+    }
+    
+    
+    
+  
 
 
 }
